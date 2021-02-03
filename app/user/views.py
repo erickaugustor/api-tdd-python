ff6c90b5a1b5387e4e0c2db2dd7620b23fef9ae4
erick <erick.augustoramos@gmail.com>
2021-02-03 19:59:47 -0300

    add the me route

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -13,3 +13,13 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSeerializer
 
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permissions_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Return authentication user"""
+        return self.request.user
